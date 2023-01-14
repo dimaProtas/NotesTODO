80fa7a256d7144f3d2a1f468a64375918cb0cb3c
dima protasevich <dima_protasevich92@mail.ru>
2023-01-14 16:14:24 +0300

    lesson_4 project pogination 10 and filter Project name

--- library/library/settings.py
+++ library/library/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-vpj9vt$x3b2t@@w*gsx$gjmo1p&4&ah77zx0w8%fhd%ojkuo*#
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -40,7 +38,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'app.apps.AppConfig',
-    'todo.apps.TodoConfig'
+    'todo.apps.TodoConfig',
+    'django_filters',
 
 ]
 
@@ -75,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'library.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -116,7 +112,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -135,4 +130,5 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
--- library/todo/views.py
+++ library/todo/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, Todo
 from .serializers import ProjectModelSerializer, TodoModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from .filters import ProjectFilter
+
+
+class ProjectLimitOffSetPagination(LimitOffsetPagination):
+    default_limit = 10
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffSetPagination
+    filterset_class = ProjectFilter
 
 
 class TodotModelViewSet(ModelViewSet):
