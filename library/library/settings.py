823ef1f488aea3ce0b18cfb47cf8746ed7921f08
dima protasevich <dima_protasevich92@mail.ru>
2023-01-20 13:00:03 +0300

    lesson_6 add Authentication project

--- library/library/settings.py
+++ library/library/settings.py
@@ -130,9 +130,12 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.BrowsableAPIRenderer', # add this first(стандартный вывод).
-            'rest_framework.renderers.AdminRenderer',  # Формат вывода Админ(более прятный)
+            'rest_framework.renderers.AdminRenderer',  # Формат вывода Админ(более прятный вид интерфеса DRF)
             'rest_framework.renderers.JSONRenderer',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
