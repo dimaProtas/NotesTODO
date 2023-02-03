f12a49f6e53bd44acfffec5575ddea9e9455cafd
dima protasevich <dima_protasevich92@mail.ru>
2023-02-03 13:07:35 +0300

    lesson_10 add graphene filter

--- library/library/schema.py
+++ library/library/schema.py
@@ -26,6 +26,8 @@ class Query(graphene.ObjectType):
     all_todos = graphene.List(TodoType)
     all_users = graphene.List(UserAbstractType)
     all_projects = graphene.List(ProjectType)
+    user_by_id = graphene.Field(UserAbstractType, id=graphene.Int(required=True))
+    todo_by_user_name = graphene.List(TodoType, name=graphene.String(required=False))
 
     def resolve_all_todos(root, info):
         return Todo.objects.all()
@@ -35,6 +37,18 @@ class Query(graphene.ObjectType):
 
     def resolve_all_projects(root, info):
         return Project.objects.all()
+
+    def resolve_user_by_id(root, info, id):
+        try:
+            return UserAbstract.objects.get(id=id)
+        except UserAbstract.DoesNotExist:
+            return None
+
+    def resolve_todo_by_user_name(root, info, name=None):
+        todo = Todo.objects.all()
+        if name:
+            todo = todo.filter(user__username=name)
+        return todo
 
 
 schema = graphene.Schema(query=Query)
